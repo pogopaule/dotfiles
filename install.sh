7bda64bf13d88508d14c6094261f652373d570c1
pogopaule <pogopaule@gmx.de>
2013-11-13 20:41:19 +0100

    fix bug in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-ln -s ./vimrc ~/.vimrc
-ln -s ./vim ~/.vim
-ln -s ./gitconfig ~/.gitconfig
-ln -s ./gitignore_global ~/.gitignore_global
-ln -s ./bash_profile ~/.bash_profile
+ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/vim ~/.vim
+ln -s $PWD/gitconfig ~/.gitconfig
+ln -s $PWD/gitignore_global ~/.gitignore_global
+ln -s $PWD/bash_profile ~/.bash_profile
 
