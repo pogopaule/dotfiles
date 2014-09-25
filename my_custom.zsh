3edb5e2c78e80bfafd6e41ffc11b0705c6690f30
pogopaule <pogopaule@gmx.de>
2014-09-25 15:29:04 +0200

    add aliases for vagrant

--- my_custom.zsh
+++ my_custom.zsh
@@ -1,1 +1,7 @@
 alias gll="git log --graph --branches --remotes --tags --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+
+alias vst="vagrant status"
+alias vup="vagrant up"
+alias vde="vagrant destroy"
+alias vdf="vagrant destroy -f"
+alias vss="vagrant ssh"
