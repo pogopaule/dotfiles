097c09c5c0e5a6211412bd089b0e1ad2dea7ab47
pogopaule <pogopaule@gmx.de>
2014-09-30 15:39:30 +0200

    change vagrant aliases

--- my_custom.zsh
+++ my_custom.zsh
@@ -1,7 +1,12 @@
 alias gll="git log --graph --branches --remotes --tags --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 
+alias v="vagrant"
 alias vst="vagrant status"
-alias vup="vagrant up"
-alias vde="vagrant destroy"
+alias vu="vagrant up"
+alias vd="vagrant destroy"
 alias vdf="vagrant destroy -f"
 alias vss="vagrant ssh"
+
+alias vb="vagrant box"
+alias vbl="vagrant box list"
+alias vbu="vagrant box update"
