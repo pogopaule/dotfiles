a42a3693f484c79efe748b567f7d1f8152a2f5bb
pogopaule <pogopaule@gmx.de>
2014-11-06 00:08:25 +0100

    change gd alias

--- my_custom.zsh
+++ my_custom.zsh
@@ -1,5 +1,6 @@
 alias gll="git log --graph --branches --remotes --tags --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 alias gfp="git fetch --prune"
+alias gd="git diff --color-words"
 
 alias v="vagrant"
 alias vst="vagrant status"
