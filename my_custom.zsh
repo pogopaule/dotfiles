712cd3455a7cb8b7ee904b80a9fc2c07f8e65940
pogopaule <pogopaule@gmx.de>
2014-12-03 14:18:58 +0100

    add alias to review changes from master to origin/master

--- my_custom.zsh
+++ my_custom.zsh
@@ -1,6 +1,7 @@
 alias gll="git log --graph --branches --remotes --tags --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 alias gfp="git fetch --prune"
 alias gd="git diff --color-words"
+alias gchanges="git log -p --reverse master..origin/master"
 
 alias v="vagrant"
 alias vst="vagrant status"
