d512f4cd949908dffd2b637b5791a7d8eef226b9
pogopaule <pogopaule@gmx.de>
2015-01-22 11:16:50 +0100

    remove custom gd alias

--- my_custom.zsh
+++ my_custom.zsh
@@ -1,6 +1,5 @@
 alias gll="git log --graph --branches --remotes --tags --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 alias gfp="git fetch --prune"
-alias gd="git diff --color-words"
 alias gchanges="git log -p --reverse master..origin/master"
 
 alias v="vagrant"
