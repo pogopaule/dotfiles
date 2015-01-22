ead8d66e893818b7a39d5fb5aa02dfd070ba5c2f
pogopaule <pogopaule@gmx.de>
2015-01-22 11:17:50 +0100

    add grema alias to rebase current branch on master

--- my_custom.zsh
+++ my_custom.zsh
@@ -1,6 +1,7 @@
 alias gll="git log --graph --branches --remotes --tags --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 alias gfp="git fetch --prune"
 alias gchanges="git log -p --reverse master..origin/master"
+alias grema="git rebase master"
 
 alias v="vagrant"
 alias vst="vagrant status"
