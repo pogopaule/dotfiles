fd6f26f968940d9dd770e69286311501777de642
pogopaule <pogopaule@posteo.de>
2022-02-13 22:07:15 +0100

    add rainbow parantheses

--- nvim/lua/pogopaule/plugins/treesitter.lua
+++ nvim/lua/pogopaule/plugins/treesitter.lua
@@ -14,7 +14,12 @@ require'nvim-treesitter.configs'.setup {
   },
   indent = {
     enable = true
-  }
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = 20000, -- Do not enable for files with more than n lines, int
+  },
 }
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
