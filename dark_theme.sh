03b3b43651c7f5169999f569966a52b7ff4cba9a
pogopaule <pogopaule@posteo.de>
2022-02-26 19:55:40 +0100

    fix

--- dark_theme.sh
+++ dark_theme.sh
@@ -2,4 +2,4 @@
 
 sed -i 's/dayfox/nordfox/' ./tmux/tmux.conf
 sed -i 's/dayfox/nordfox/' ./alacritty/alacritty.yml
-sed -i 's/fox = "dayfox"/fox = "nordfox/' ./nvim/lua/pogopaule/plugins/simple_setups.lua
+sed -i 's/fox = "dayfox"/fox = "nordfox"/' ./nvim/lua/pogopaule/plugins/simple_setups.lua
--- light_theme.sh
+++ light_theme.sh
@@ -2,4 +2,4 @@
 
 sed -i 's/nordfox/dayfox/' ./tmux/tmux.conf
 sed -i 's/nordfox/dayfox/' ./alacritty/alacritty.yml
-sed -i 's/fox = "nordfox"/fox = "dayfox/' ./nvim/lua/pogopaule/plugins/simple_setups.lua
+sed -i 's/fox = "nordfox"/fox = "dayfox"/' ./nvim/lua/pogopaule/plugins/simple_setups.lua
