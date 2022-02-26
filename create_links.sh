43c3087c2896f31496f9811a9ab147b486a10785
pogopaule <pogopaule@posteo.de>
2022-02-26 19:53:44 +0100

    updates

--- create_links.sh
+++ create_links.sh
@@ -1,13 +1,9 @@
 #! /bin/bash
 
-# ln -s $PWD/vimrc ~/.vimrc
-ln -s $PWD/vrapperrc ~/.vrapperrc
 ln -s $PWD/gitconfig ~/.gitconfig
 ln -s $PWD/gitignore_global ~/.gitignore_global
-ln -s $PWD/bash_profile ~/.bash_profile
-ln -s $PWD/agignore ~/.agignore
-ln -s $PWD/tmux.conf ~/.tmux.conf
+ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
 ln -s $PWD/zshrc ~/.zshrc
-# TODO: replace with nvim lua config
-ln -s $PWD/init.vim ~/.config/nvim/init.vim
+ln -s $PWD/nvim ~/.config/nvim
 ln -s $PWD/alacritty/alacritty.yml ~/.alacritty.yml
+ln -s $PWD/starship.toml ~/.config/starship.toml
