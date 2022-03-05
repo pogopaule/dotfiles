c048c68e7379a9c8414531c450efe11eac9c8aba
pogopaule <pogopaule@posteo.de>
2022-03-05 11:53:38 +0100

    add vale for text linting

--- nvim/lua/pogopaule/lsp/null-ls.lua
+++ nvim/lua/pogopaule/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,14 +11,18 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		-- formatting.black.with({ extra_args = { "--fast" } }),
-		-- formatting.stylua,
+  debug = false,
+  sources = {
+    -- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    -- formatting.black.with({ extra_args = { "--fast" } }),
+    -- formatting.stylua,
     -- diagnostics.flake8
     formatting.eslint_d,
     diagnostics.eslint_d,
     code_actions.eslint_d,
-	},
+
+    diagnostics.vale.with({
+      filetypes = { "markdown", "text", "gitcommit"}
+    }),
+  },
 })
