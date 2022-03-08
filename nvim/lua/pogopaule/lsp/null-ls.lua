a529c4d519e4218807089226176ba297212533fb
pogopaule <pogopaule@posteo.de>
2022-03-08 16:51:36 +0100

    only use eslint and prettier if config files are present

--- nvim/lua/pogopaule/lsp/null-ls.lua
+++ nvim/lua/pogopaule/lsp/null-ls.lua
@@ -10,16 +10,35 @@ local diagnostics = null_ls.builtins.diagnostics
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
 local code_actions = null_ls.builtins.code_actions
 
+local eslintConfig = {
+  condition = function(utils)
+    return utils.root_has_file({
+      '.eslintrc.cjs',
+      '.eslintrc.js',
+      '.eslintrc.json',
+      '.eslintrc.yaml',
+      '.eslintrc.yml',
+    })
+  end
+}
+
+local prettierConfig = {
+  condition = function(utils)
+    return utils.root_has_file('.prettierrc')
+  end
+}
+
 null_ls.setup({
   debug = false,
   sources = {
-    -- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
     -- formatting.black.with({ extra_args = { "--fast" } }),
     -- formatting.stylua,
     -- diagnostics.flake8
-    formatting.eslint_d,
-    diagnostics.eslint_d,
-    code_actions.eslint_d,
+    formatting.eslint_d.with(eslintConfig),
+    diagnostics.eslint_d.with(eslintConfig),
+    code_actions.eslint_d.with(eslintConfig),
+
+    formatting.prettier.with(prettierConfig),
 
     diagnostics.vale.with({
       filetypes = { "markdown", "text", "gitcommit"},
