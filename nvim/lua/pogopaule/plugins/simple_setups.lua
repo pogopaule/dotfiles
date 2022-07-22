06e5054a54aeb82f54ab76861482be3965ea4981
pogopaule <pogopaule@posteo.de>
2022-07-22 15:44:04 +0200

    fix(nvim): don't show trailing spaces on startup screen

--- nvim/lua/pogopaule/plugins/simple_setups.lua
+++ nvim/lua/pogopaule/plugins/simple_setups.lua
@@ -20,10 +20,16 @@ require("retrail").setup {
   hlgroup = "Substitute",
   -- Enabled filetypes.
   filetype = {
+    -- Strictly enable only on `include`ed filetypes. When false, only disabled
+    -- on an `exclude`ed filetype.
+    strict = false,
+    -- Included filetype list.
+    include = {},
     -- Excluded filetype list. Overrides `include` list.
     exclude = {
-      "help",
-      "alpha"
+      "",
+      "alpha",
+      "help"
     },
   },
   -- Trim on write behaviour.
