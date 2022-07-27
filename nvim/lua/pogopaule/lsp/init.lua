44935be30f56cc39fb462b5222673318aa1b9997
pogopaule <pogopaule@posteo.de>
2022-07-27 12:43:27 +0200

    feat(nvim): set nice signs for LSP

--- nvim/lua/pogopaule/lsp/init.lua
+++ nvim/lua/pogopaule/lsp/init.lua
@@ -7,6 +7,20 @@ require('pogopaule.lsp.mason')
 require('pogopaule.lsp.null-ls')
 require('lspsaga').init_lsp_saga() -- https://github.com/glepnir/lspsaga.nvim
 
+
+local sign = function(opts)
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+
+sign({ name = 'DiagnosticSignError', text = '✘' })
+sign({ name = 'DiagnosticSignWarn', text = '▲' })
+sign({ name = 'DiagnosticSignHint', text = '⚑' })
+sign({ name = 'DiagnosticSignInfo', text = '' })
+
 -- disable virtual text for diagnostics
 vim.diagnostic.config({
   virtual_text = false,
