89793ba9e55b18bda15456c07382eb57efafdbe7
pogopaule <pogopaule@posteo.de>
2022-08-26 16:38:27 +0200

    fix(nvim): make signature_help work again

--- nvim/lua/pogopaule/keymaps.lua
+++ nvim/lua/pogopaule/keymaps.lua
@@ -113,7 +113,7 @@ map('n', '<leader>a', require('lspsaga.codeaction').code_action, opts)
 map('n', '<leader>cp', '<cmd>Lspsaga preview_definition<CR>', opts)
 map('n', '<leader>e', '<cmd>Lspsaga show_line_diagnostics<CR>', opts)
 map('n', 'K', require('lspsaga.hover').render_hover_doc, opts)
-map('n', '<leader>cs', require('lspsaga.signaturehelp').signature_help, opts)
+map('n', '<leader>cs', vim.lsp.buf.signature_help, opts)
 map('n', '<leader>fr', require('lspsaga.finder').lsp_finder, opts)
 
 -- tyru/open-browser.vim
