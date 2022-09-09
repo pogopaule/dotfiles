859d41719cdc9b1d230858b6e96cd70382f04fd0
pogopaule <pogopaule@posteo.de>
2022-09-09 21:46:51 +0200

    feat(nvim): install only subset of TS languages

--- nvim/lua/pogopaule/plugins/treesitter.lua
+++ nvim/lua/pogopaule/plugins/treesitter.lua
@@ -1,5 +1,9 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = 'all',
+  ensure_installed = {
+    'bash', 'clojure', 'cmake', 'css', 'dockerfile', 'fennel', 'gitattributes', 'gitignore', 'go', 'graphql', 'haskell', 'html', 'http',
+    'java', 'javascript', 'jsdoc', 'json', 'json5', 'JSON', 'kotlin', 'lua', 'make', 'markdown', 'markdown_inline', 'nix', 'python',
+    'Tree-sitter', 'regex', 'ruby', 'rust', 'scss', 'sql', 'svelte', 'todotxt', 'toml', 'tsx', 'typescript', 'vim', 'vue', 'yaml',
+  },
   highlight = {
     enable = true,
   },
