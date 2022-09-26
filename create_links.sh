7a438e440b31bbb4b6de10b5ee30ec0661245379
pogopaule <pogopaule@posteo.de>
2022-09-26 15:28:48 +0200

    cleanup(nix): remove ported files

--- create_links.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-ln -s $PWD/gitconfig ~/.gitconfig
-ln -s $PWD/gitignore_global ~/.gitignore_global
-ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
-ln -s $PWD/zshrc ~/.zshrc
-ln -s $PWD/nvim ~/.config/nvim
-ln -s $PWD/alacritty/alacritty.yml ~/.alacritty.yml
