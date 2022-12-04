817064bebb68f85777475057e0623b35697d1ebe
pogopaule <pogopaule@posteo.de>
2022-12-04 22:11:21 +0100

    refactor(nvim): plugins.lua

--- nvim/lua/pogopaule/plugins.lua
+++ nvim/lua/pogopaule/plugins.lua
@@ -13,26 +13,42 @@
 -- https://github.com/andythigpen/nvim-coverage display test coverage
 
 local function incubator(use)
-  use 'https://github.com/dkarter/bullets.vim' -- fixes indentation problems with bullet lists
-  use 'https://github.com/nvim-treesitter/playground' -- treesitter playground, show syntax tree
-  use 'https://github.com/dbeniamine/cheat.sh-vim' -- cheat.sh integration
-  use { 'https://github.com/pwntester/octo.nvim', config = function() require('octo').setup() end } -- github integration
-  -- use 'https://github.com/editorconfig/editorconfig-vim' -- editorconfig integration
-  -- use 'https://github.com/jamestthompson3/nvim-remote-containers' -- use vscode's container definitions in nvim
-  -- use 'https://github.com/dstein64/vim-startuptime' -- measure startup time
+  -- fixes indentation problems with bullet lists
+  use 'https://github.com/dkarter/bullets.vim'
 
-  use { 'https://github.com/ldelossa/gh.nvim', -- Code reviews in vim
+  -- treesitter playground, show syntax tree
+  use 'https://github.com/nvim-treesitter/playground'
+
+  -- cheat.sh integration
+  use 'https://github.com/dbeniamine/cheat.sh-vim'
+
+  -- github integration
+  use { 'https://github.com/pwntester/octo.nvim', config = function() require('octo').setup() end }
+
+  -- -- editorconfig integration
+  -- use 'https://github.com/editorconfig/editorconfig-vim'
+
+  -- -- use vscode's container definitions in nvim
+  -- use 'https://github.com/jamestthompson3/nvim-remote-containers'
+
+  -- -- measure startup time
+  -- use 'https://github.com/dstein64/vim-startuptime'
+
+  -- Code reviews in vim
+  use { 'https://github.com/ldelossa/gh.nvim',
     requires = { { 'https://github.com/ldelossa/litee.nvim' } }
   }
 
-  use { 'https://github.com/potamides/pantran.nvim', config = function() -- translate text
+  -- translate text
+  use { 'https://github.com/potamides/pantran.nvim', config = function()
     require('pantran').setup {
       default_engine = 'deepl',
       engines = { deepl = { default_target = 'de' } },
     }
   end }
 
-  use { 'https://github.com/monaqa/dial.nvim', config = function() -- enhanced inc/dec
+  -- enhanced inc/dec
+  use { 'https://github.com/monaqa/dial.nvim', config = function()
     local augend = require("dial.augend")
     require("dial.config").augends:register_group {
       default = {
@@ -48,17 +64,28 @@ local function incubator(use)
 end
 
 local function basics(use)
-  use 'https://github.com/wbthomason/packer.nvim' -- Have packer manage itself
-  use 'https://github.com/nvim-lua/popup.nvim' -- Popup API from vim in neovim
-  use 'https://github.com/nvim-lua/plenary.nvim' -- Lua functions
-  use 'https://github.com/antoinemadec/FixCursorHold.nvim' -- fix CursorHold performance
+  -- Have packer manage itself
+  use 'https://github.com/wbthomason/packer.nvim'
+
+  -- Popup API from vim in neovim
+  use 'https://github.com/nvim-lua/popup.nvim'
+
+  -- Lua functions
+  use 'https://github.com/nvim-lua/plenary.nvim'
+
+  -- fix CursorHold performance
+  use 'https://github.com/antoinemadec/FixCursorHold.nvim'
 end
 
 local function layoutAndWindows(use)
-  use { 'https://github.com/kazhala/close-buffers.nvim', config = function() require('close_buffers').setup() end } -- helpers to close buffers, used by bufferline
-  use { 'https://github.com/kyazdani42/nvim-web-devicons', config = function() require('nvim-web-devicons').setup() end } -- Icon font
+  -- helpers to close buffers, used by bufferline
+  use { 'https://github.com/kazhala/close-buffers.nvim', config = function() require('close_buffers').setup() end }
 
-  use { 'https://github.com/kyazdani42/nvim-tree.lua', config = function() -- File Explorer
+  -- Icon font
+  use { 'https://github.com/kyazdani42/nvim-web-devicons', config = function() require('nvim-web-devicons').setup() end }
+
+  -- File Explorer
+  use { 'https://github.com/kyazdani42/nvim-tree.lua', config = function()
     require('nvim-tree').setup({
       filters = {
         dotfiles = true,
@@ -78,11 +105,13 @@ local function layoutAndWindows(use)
     vim.g.nvim_tree_special_files = {}
   end }
 
-  use { 'https://github.com/goolord/alpha-nvim', config = function() -- Start screen
+  -- Start screen
+  use { 'https://github.com/goolord/alpha-nvim', config = function()
     require('alpha').setup(require('alpha.themes.startify').config)
   end }
 
-  use { 'https://github.com/nvim-lualine/lualine.nvim', config = function() -- Status bar
+  -- Status bar
+  use { 'https://github.com/nvim-lualine/lualine.nvim', config = function()
     require('lualine').setup({
       options = {
         disabled_filetypes = {
@@ -92,7 +121,8 @@ local function layoutAndWindows(use)
     })
   end }
 
-  use { 'https://github.com/EdenEast/nightfox.nvim', run = ':NightfoxCompile', config = function() -- nightfox theme
+  -- nightfox theme
+  use { 'https://github.com/EdenEast/nightfox.nvim', run = ':NightfoxCompile', config = function()
     local nightfox = require('nightfox')
     nightfox.setup({
       options = {
@@ -109,8 +139,9 @@ local function layoutAndWindows(use)
     end
   end }
 
+  -- buffers as tabs
   use { 'https://github.com/akinsho/bufferline.nvim', config = function()
-    require('bufferline').setup({ -- buffers as tabs
+    require('bufferline').setup({
       options = {
         offsets = { {
           filetype = 'NvimTree',
@@ -122,16 +153,21 @@ local function layoutAndWindows(use)
     })
   end }
 
-  use { 'https://github.com/rcarriga/nvim-notify', config = function() -- popup messages
+  -- popup messages
+  use { 'https://github.com/rcarriga/nvim-notify', config = function()
     vim.notify = require("notify")
   end }
 end
 
 local function movingAround(use)
-  use { 'https://github.com/ggandor/leap.nvim', config = function() require('leap').add_default_mappings() end } -- Jump in text
-  use 'https://github.com/nvim-telescope/telescope.nvim' -- Find, Filter, Preview, Pick
+  -- Jump in text
+  use { 'https://github.com/ggandor/leap.nvim', config = function() require('leap').add_default_mappings() end }
 
-  use { 'https://github.com/nvim-telescope/telescope-ui-select.nvim', config = function() -- use telescope to select options
+  -- Find, Filter, Preview, Pick
+  use 'https://github.com/nvim-telescope/telescope.nvim'
+
+  -- use telescope to select options
+  use { 'https://github.com/nvim-telescope/telescope-ui-select.nvim', config = function()
     require('telescope').setup {
       extensions = {
         ['ui-select'] = { require('telescope.themes').get_dropdown {} }
@@ -144,13 +180,23 @@ local function movingAround(use)
 end
 
 local function misc(use)
-  use 'https://github.com/godlygeek/tabular' -- Tabularize
-  use 'http://github.com/lewis6991/impatient.nvim' -- speed up startup
-  use 'https://github.com/christoomey/vim-tmux-navigator' -- Seamless jumping between vim and tmux
-  use 'https://github.com/airblade/vim-rooter' -- Changes Vim working directory to project root
-  use { 'https://github.com/folke/which-key.nvim', config = function() require('which-key').setup() end } -- shows what to type after a prefix
+  -- Tabularize
+  use 'https://github.com/godlygeek/tabular'
 
-  use { 'https://github.com/zakharykaplan/nvim-retrail', config = function() -- Show and remove unwanted whitespaces
+  -- speed up startup
+  use 'http://github.com/lewis6991/impatient.nvim'
+
+  -- Seamless jumping between vim and tmux
+  use 'https://github.com/christoomey/vim-tmux-navigator'
+
+  -- Changes Vim working directory to project root
+  use 'https://github.com/airblade/vim-rooter'
+
+  -- shows what to type after a prefix
+  use { 'https://github.com/folke/which-key.nvim', config = function() require('which-key').setup() end }
+
+  -- Show and remove unwanted whitespaces
+  use { 'https://github.com/zakharykaplan/nvim-retrail', config = function()
     require('retrail').setup {
       hlgroup = 'Substitute',
       filetype = {
@@ -162,13 +208,15 @@ local function misc(use)
     }
   end }
 
-  use { 'https://github.com/karb94/neoscroll.nvim', config = function() -- smooth scrolling
+  -- smooth scrolling
+  use { 'https://github.com/karb94/neoscroll.nvim', config = function()
     require('neoscroll').setup({
       mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
     })
   end }
 
-  use { 'https://github.com/tyru/open-browser.vim', config = function() -- open url or word as search in browser
+  -- open url or word as search in browser
+  use { 'https://github.com/tyru/open-browser.vim', config = function()
     vim.cmd([[
       " disable netrw's gx mapping.
       let g:netrw_nogx = 1
@@ -179,24 +227,48 @@ local function misc(use)
 end
 
 local function coding(use)
-  use { 'https://github.com/nvim-treesitter/nvim-treesitter', run = ':TSUpdate' } -- Nvim Treesitter configurations and abstraction layer
-  use 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects' -- Text objects like functions and variables
-  use 'https://github.com/andymass/vim-matchup' -- extends %
-  use { 'https://github.com/numToStr/Comment.nvim', config = require('Comment').setup() } -- Comments
-  use { 'https://github.com/norcalli/nvim-colorizer.lua', config = function() require('colorizer').setup() end } -- Colorize hex colors
-  use { 'https://github.com/folke/todo-comments.nvim', config = function() require('todo-comments').setup() end } -- Highlight, list and search todo comments
-  use 'https://github.com/preservim/vim-markdown' -- Markdown
-  use { 'https://github.com/ThePrimeagen/refactoring.nvim', config = function() require('refactoring').setup() end } -- Refactoring
-  use 'https://github.com/davidgranstrom/nvim-markdown-preview' -- Preview markdown, requires live-preview and pandoc to be installed
-  use 'https://github.com/b0o/schemastore.nvim' -- JSON schema awareness, gives LSP completions for e.g. package.json
-  use 'https://github.com/nvim-neotest/neotest-python'
-  use { 'https://github.com/andrewferrier/debugprint.nvim', config = function() require('debugprint').setup() end } -- print() debugging
+  -- Nvim Treesitter configurations and abstraction layer
+  use { 'https://github.com/nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
-  use { 'https://github.com/kylechui/nvim-surround', config = function() -- easily surround with brackets
+  -- Text objects like functions and variables
+  use 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects'
+
+  -- extends %
+  use 'https://github.com/andymass/vim-matchup'
+
+  -- Comments
+  use { 'https://github.com/numToStr/Comment.nvim', config = require('Comment').setup() }
+
+  -- Colorize hex colors
+  use { 'https://github.com/norcalli/nvim-colorizer.lua', config = function() require('colorizer').setup() end }
+
+  -- Highlight, list and search todo comments
+  use { 'https://github.com/folke/todo-comments.nvim', config = function() require('todo-comments').setup() end }
+
+  -- Markdown
+  use 'https://github.com/preservim/vim-markdown'
+
+  -- Refactoring
+  use { 'https://github.com/ThePrimeagen/refactoring.nvim', config = function() require('refactoring').setup() end }
+
+  -- Preview markdown, requires live-preview and pandoc to be installed
+  use 'https://github.com/davidgranstrom/nvim-markdown-preview'
+
+  -- JSON schema awareness, gives LSP completions for e.g. package.json
+  use 'https://github.com/b0o/schemastore.nvim'
+
+  use 'https://github.com/nvim-neotest/neotest-python'
+
+  -- print() debugging
+  use { 'https://github.com/andrewferrier/debugprint.nvim', config = function() require('debugprint').setup() end }
+
+  -- easily surround with brackets
+  use { 'https://github.com/kylechui/nvim-surround', config = function()
     require('nvim-surround').setup()
   end }
 
-  use { 'https://github.com/windwp/nvim-autopairs', config = function() -- Insert matching quote, brackets, etc.
+  -- Insert matching quote, brackets, etc.
+  use { 'https://github.com/windwp/nvim-autopairs', config = function()
     require('nvim-autopairs').setup({ fast_wrap = {} })
     -- https://github.com/windwp/nvim-autopairs#you-need-to-add-mapping-cr-on-nvim-cmp-setupcheck-readmemd-on-nvim-cmp-repo
     -- If you want insert `(` after select function or method item
@@ -208,7 +280,8 @@ local function coding(use)
     )
   end }
 
-  use { 'https://github.com/jpalardy/vim-slime', config = function() -- Send line to tmux
+  -- Send line to tmux
+  use { 'https://github.com/jpalardy/vim-slime', config = function()
     vim.cmd([[
     let g:slime_target = "tmux"
     let g:slime_default_config = {"socket_name": "default", "target_pane": "{last}"}
@@ -216,7 +289,8 @@ local function coding(use)
     ]])
   end }
 
-  use { 'https://github.com/lukas-reineke/indent-blankline.nvim', config = function() -- indentation guides
+  -- indentation guides
+  use { 'https://github.com/lukas-reineke/indent-blankline.nvim', config = function()
     require("indent_blankline").setup { show_current_context = true }
   end }
 
@@ -241,21 +315,43 @@ local function debugging(use)
 end
 
 local function lsp(use)
-  use 'https://github.com/neovim/nvim-lspconfig' -- LSP support
-  use 'https://github.com/glepnir/lspsaga.nvim' -- The neovim language-server-client UI
-  use 'https://github.com/onsails/lspkind-nvim' -- vscode-like pictograms for neovim lsp completion items
-  use 'https://github.com/jose-elias-alvarez/null-ls.nvim' -- LSP bridge for linters and others
-  use 'https://github.com/ray-x/lsp_signature.nvim' -- LSP signature hint as you type
-  use { 'https://github.com/j-hui/fidget.nvim', config = function() require('fidget').setup() end } -- Show LSP progress
-  use { 'https://github.com/folke/trouble.nvim', config = function() require('trouble').setup() end } -- Nicer diagnostics
-  use 'https://github.com/mfussenegger/nvim-jdtls' -- LSP and DAP for java
+  -- LSP support
+  use 'https://github.com/neovim/nvim-lspconfig'
+
+  -- The neovim language-server-client UI
+  use 'https://github.com/glepnir/lspsaga.nvim'
+
+  -- vscode-like pictograms for neovim lsp completion items
+  use 'https://github.com/onsails/lspkind-nvim'
+
+  -- LSP bridge for linters and others
+  use 'https://github.com/jose-elias-alvarez/null-ls.nvim'
+
+  -- LSP signature hint as you type
+  use 'https://github.com/ray-x/lsp_signature.nvim'
+
+  -- Show LSP progress
+  use { 'https://github.com/j-hui/fidget.nvim', config = function() require('fidget').setup() end }
+
+  -- Nicer diagnostics
+  use { 'https://github.com/folke/trouble.nvim', config = function() require('trouble').setup() end }
+
+  -- LSP and DAP for java
+  use 'https://github.com/mfussenegger/nvim-jdtls'
 end
 
 local function git(use)
-  use { 'https://github.com/lewis6991/gitsigns.nvim', config = function() require('gitsigns').setup() end } -- git decoration for buffers
-  use 'https://github.com/tpope/vim-fugitive' -- Git
-  use 'https://github.com/tpope/vim-rhubarb' -- Adds Github to futitive, e.g. Gbrowse
-  use { 'https://github.com/ruifm/gitlinker.nvim', config = function() require('gitlinker').setup() end } -- create github permalink via <leader>gy
+  -- git decoration for buffers
+  use { 'https://github.com/lewis6991/gitsigns.nvim', config = function() require('gitsigns').setup() end }
+
+  -- Git
+  use 'https://github.com/tpope/vim-fugitive'
+
+  -- Adds Github to futitive, e.g. Gbrowse
+  use 'https://github.com/tpope/vim-rhubarb'
+
+  -- create github permalink via <leader>gy
+  use { 'https://github.com/ruifm/gitlinker.nvim', config = function() require('gitlinker').setup() end }
 end
 
 local function completion(use)
@@ -273,7 +369,8 @@ local function snippets(use)
   use 'https://github.com/rafamadriz/friendly-snippets'
 end
 
--- -- Autocommand that reloads neovim whenever you save the plugins.lua file
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+--
 -- local group = vim.api.nvim_create_augroup('packer_user_config', { clear = true })
 -- vim.api.nvim_create_autocmd('BufWritePost', {
 --   command = 'source <afile> | PackerSync',
