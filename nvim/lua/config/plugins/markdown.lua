b06aa19a4ed8b003c536c8e5703a22152fe3a8cc
pogopaule <pogopaule@posteo.de>
2023-01-14 17:30:06 +0100

    chore(nvim): remove vim-markdown plugin
    caused problem with opening links via gx. Not sure why it was there in
    the first place

--- nvim/lua/config/plugins/markdown.lua
+++ nvim/lua/config/plugins/markdown.lua
@@ -1,10 +1,4 @@
 return {
-  -- Markdown
-  {
-    'https://github.com/preservim/vim-markdown',
-    ft = 'markdown',
-  },
-
   -- Preview markdown, requires live-preview and pandoc to be installed
   {
     'https://github.com/davidgranstrom/nvim-markdown-preview',
