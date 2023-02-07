ebeae453b360d92bc6e1f02e261e4944d730ac5c
pogopaule <pogopaule@posteo.de>
2023-02-07 17:27:27 +0000

    feat(nvim): map folding to space

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -20,6 +20,10 @@ map({'n', 'v'}, '<A-l>', '<CMD>tabnext<CR>', opts)
 map({'n', 'v'}, '<C-w>', "<CMD>tabclose<CR>", opts)
 
 
+-- map folding to space
+map('n', '<space>', 'za', opts)
+
+
 -- exit insert mode by pressing jk
 map('i', 'jk', '<ESC>', opts)
 
