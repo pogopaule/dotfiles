a2989337a6675a358ae83f1e8066bd62027bd207
pogopaule <pogopaule@posteo.de>
2023-02-10 18:31:25 +0100

    feat(nix): add wezterm

--- nix/home-desktop.nix
+++ nix/home-desktop.nix
@@ -17,10 +17,34 @@
     ];
   };
 
+  programs = {
+    wezterm = {
+      enable = true;
+      extraConfig = ''
+        return {
+          hide_tab_bar_if_only_one_tab = true,
+          font_size = 16.0,
+          font = wezterm.font('Hack Nerd Font'),
+          color_scheme = ${ if darkTheme then "'nordfox'" else "'dawnfox'" },
+          colors = {
+            cursor_bg = '#ff00ff',
+            cursor_fg = '#ffffff',
+          },
+          window_padding = {
+            left = 0,
+            right = 0,
+            top = 0,
+            bottom = 0,
+          },
+        }
+      '';
+    };
+  };
+
   fonts.fontconfig.enable = true; # for Hack Nerdfont
 
   imports = [
-    ( import ../alacritty { inherit darkTheme; })
+    (import ../alacritty { inherit darkTheme; })
   ];
 
   programs = {
