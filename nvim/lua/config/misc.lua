1f5d48cc2fc73a1596b856d0c6d6a2487b1eb84f
pogopaule <pogopaule@posteo.de>
2023-03-08 17:51:59 +0100

    feat(nvim): make DeleteEmptyLines work for ranges

--- nvim/lua/config/misc.lua
+++ nvim/lua/config/misc.lua
@@ -2,7 +2,17 @@
 vim.api.nvim_create_user_command('PasteGeo', 'normal "+p0f:lldf@f,f,DF,xDj0$p', {})
 
 -- small helper commands
-vim.api.nvim_create_user_command('DeleteEmptyLines', 'g/^$/d ', {})
+vim.api.nvim_create_user_command(
+  'DeleteEmptyLines',
+  function(arg)
+    if arg.range ~= 0 then
+      vim.api.nvim_exec(string.format("%s,%sg/^$/d", arg.line1, arg.line2), {});
+    else
+      vim.api.nvim_exec("g/^$/d", {});
+    end
+  end,
+  { range = true }
+)
 
 
 -- highlight yanked region
