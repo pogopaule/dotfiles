7d8a00605a51ba3ab2800099b96821d6acf1966e
pogopaule <fabian.schwarzer@node.energy>
2023-07-04 22:43:39 +0200

    feat(nvim): detect system theme for mac

--- nvim/lua/config/plugins/colorscheme.lua
+++ nvim/lua/config/plugins/colorscheme.lua
@@ -15,10 +15,26 @@ return {
           },
         }
       })
-      if os.getenv("TERMINAL_THEME") == 'light' then
-        vim.cmd('colorscheme dawnfox')
+      if vim.loop.os_uname().sysname == 'Darwin' then
+        local handle = io.popen("defaults read -g AppleInterfaceStyle")
+        local result = handle:read("*a")
+        handle:close()
+
+        result = string.gsub(result, '%s+', '')
+
+        if result == 'Dark' then
+          vim.cmd('colorscheme nordfox')
+        else
+          vim.cmd('colorscheme dawnfox')
+        end
+
       else
-        vim.cmd('colorscheme nordfox')
+        if os.getenv("TERMINAL_THEME") == 'light' then
+          vim.cmd('colorscheme dawnfox')
+        else
+          vim.cmd('colorscheme nordfox')
+        end
+
       end
     end
   },
