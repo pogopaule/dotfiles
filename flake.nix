ea78e7fc8d8d35cde8c6f3752c59e7117901cdcf
pogopaule <pogopaule@posteo.de>
2023-07-10 21:56:55 +0200

    chore(nix): update to 23

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "pogopaule's system config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.11";
+    nixpkgs.url = "nixpkgs/nixos-23.05";
     nixpkgs-master.url = "nixpkgs/master";
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
