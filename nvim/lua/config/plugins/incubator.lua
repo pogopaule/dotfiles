fccb1ac1cd743abbb5d81bb516b6b548fd292ca2
pogopaule <fabian.schwarzer@node.energy>
2023-07-25 19:06:50 +0200

    feat(nvim): make neotest work and add keybindings

--- nvim/lua/config/plugins/incubator.lua
+++ nvim/lua/config/plugins/incubator.lua
@@ -53,10 +53,24 @@ return {
     dependencies = {
       { 'https://github.com/nvim-neotest/neotest-python' },
     },
+    ft = { 'python' },
+    keys = {
+      { '<leader>TT', '<CMD>lua require("neotest").run.run()<CR>', desc = 'Nearest' },
+      { '<leader>Tf', '<CMD>lua require("neotest").run.run(vim.fn.expand("%"))<CR>', desc = 'File' },
+      { '<leader>Ta', '<CMD>lua require("neotest").run.attach()<CR>', desc = 'Attach' },
+      { '<leader>Tx', '<CMD>lua require("neotest").run.attach()<CR>', desc = 'Stop Nearest' },
+      { '<leader>Ts', '<CMD>lua require("neotest").summary.toggle()<CR>', desc = 'Toggle Summary' },
+    },
     config = function()
       require("neotest").setup({
         adapters = {
-          require("neotest-python"),
+          require("neotest-python")({
+            python = "python",
+            runner = "pytest",
+          }),
+        },
+        quickfix = {
+          open = false,
         },
       })
     end,
