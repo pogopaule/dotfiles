2a5eab86c91d641a64f7371c0a5b69ec10fa3600
pogopaule <fabian.schwarzer@node.energy>
2023-07-27 22:12:29 +0200

    refactor(zsh): move toggle_theme.sh to function

--- zsh/default.nix
+++ zsh/default.nix
@@ -46,6 +46,20 @@
 
       # https://www.npmjs.com/package/@githubnext/github-copilot-cli
       eval "$(github-copilot-cli alias -- "$0")"
+
+      function toggleTheme() {
+        if grep -q "dark" ~/.theme; then
+            echo "light" > ~/.theme
+            tmux source-file ~/dotfiles/tmux/dawnfox.conf
+            export FZF_DEFAULT_OPTS='--color=light'
+        else
+            echo "dark" > ~/.theme
+            tmux source-file ~/dotfiles/tmux/nordfox.conf
+            export FZF_DEFAULT_OPTS='--color=dark'
+        fi
+
+        sudo touch ~/.config/wezterm/wezterm.lua
+      }
     '';
     oh-my-zsh = {
       enable = true;
