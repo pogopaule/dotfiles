df527872848d7895eece98d2093b953b225d2e3c
pogopaule <fabian.schwarzer@node.energy>
2023-08-02 21:57:08 +0200

    fix(nvim): mappings for neotest

--- nvim/lua/config/plugins/incubator.lua
+++ nvim/lua/config/plugins/incubator.lua
@@ -51,11 +51,11 @@ return {
     },
     ft = { 'python' },
     keys = {
-      { '<leader>TT', '<CMD>lua require("neotest").run.run()<CR>',                   desc = 'Nearest' },
-      { '<leader>Tf', '<CMD>lua require("neotest").run.run(vim.fn.expand("%"))<CR>', desc = 'File' },
-      { '<leader>Ta', '<CMD>lua require("neotest").run.attach()<CR>',                desc = 'Attach' },
-      { '<leader>Tx', '<CMD>lua require("neotest").run.attach()<CR>',                desc = 'Stop Nearest' },
-      { '<leader>Ts', '<CMD>lua require("neotest").summary.toggle()<CR>',            desc = 'Toggle Summary' },
+      { '<leader>TT', '<CMD>write | lua require("neotest").run.run()<CR>',                     desc = 'Nearest' },
+      { '<leader>Tf', '<CMD>write | lua require("neotest").run.run(vim.fn.expand("%"))<CR>',   desc = 'File' },
+      { '<leader>Ta', '<CMD>lua require("neotest").run.attach()<CR>',                          desc = 'Attach' },
+      { '<leader>Tx', '<CMD>lua require("neotest").run.stop()<CR>',                            desc = 'Stop Nearest' },
+      { '<leader>Ts', '<CMD>lua require("neotest").summary.toggle()<CR>',                      desc = 'Toggle Summary' },
     },
     config = function()
       require("neotest").setup({
