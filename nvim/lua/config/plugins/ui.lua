03d1b4497f2fb163a700f6390a623676af6328a6
pogopaule <fabian.schwarzer@node.energy>
2023-08-24 22:58:16 +0200

    feat(nvim): remove header spacing for smaller screens

--- nvim/lua/config/plugins/ui.lua
+++ nvim/lua/config/plugins/ui.lua
@@ -13,14 +13,7 @@ return {
     lazy = false,
     config = function()
       local startify = require('alpha.themes.startify')
-      startify.section.header.val = {
-        [[                                                     ]],
-        [[                                                     ]],
-        [[                                                     ]],
-        [[                                                     ]],
-        [[                                                     ]],
-        [[                                                     ]],
-      }
+      startify.section.header.val = {}
       require('alpha').setup(startify.config)
     end
   },
