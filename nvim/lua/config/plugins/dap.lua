db3a4a6dde5098aa68cc57aea1058e4a89388aa8
pogopaule <fabian.schwarzer@node.energy>
2023-09-18 13:18:26 +0200

    fix(nvim): python config for dap

--- nvim/lua/config/plugins/dap.lua
+++ nvim/lua/config/plugins/dap.lua
@@ -66,14 +66,21 @@ return {
 
       dap.configurations.python = {
         {
+          type = 'python',
+          request = 'attach',
+          name = 'Attach to running python process',
+          host = '127.0.0.1',
+          port = 8888,
+        },
+        {
           -- The first three options are required by nvim-dap
-          type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
-          request = 'launch';
-          name = 'Launch file';
+          type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+          request = 'launch',
+          name = 'Launch file',
 
           -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
 
-          program = '${file}'; -- This configuration will launch the current file if used.
+          program = '${file}', -- This configuration will launch the current file if used.
           pythonPath = function()
             -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
             -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
@@ -86,7 +93,7 @@ return {
             else
               return 'python'
             end
-          end;
+          end,
         },
       }
 
