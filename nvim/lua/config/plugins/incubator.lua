4068dae339d9d67e78b0d9520740481f47069252
pogopaule <fabian.schwarzer@node.energy>
2024-02-11 12:54:22 +0100

    chore(nvim): remove unused plugins

--- nvim/lua/config/plugins/incubator.lua
+++ nvim/lua/config/plugins/incubator.lua
@@ -1,11 +1,7 @@
 return {
   {
     'https://github.com/rest-nvim/rest.nvim',
-    config = true;
-  },
-  {
-    'https://github.com/subnut/nvim-ghost.nvim',
-    lazy = false,
+    config = true,
   },
   {
     'https://github.com/ThePrimeagen/harpoon',
@@ -31,21 +27,6 @@ return {
     'https://github.com/sindrets/diffview.nvim',
     lazy = false,
     conifg = true,
-  },
-  {
-    'https://github.com/james1236/backseat.nvim',
-    lazy = false,
-    config =
-    {
-      openai_model_id = 'gpt-3.5-turbo', --gpt-4 (If you do not have access to a model, it says "The model does not exist")
-      -- language = 'english', -- Such as 'japanese', 'french', 'pirate', 'LOLCAT'
-      -- split_threshold = 100,
-      -- additional_instruction = "Respond snarkily", -- (GPT-3 will probably deny this request, but GPT-4 complies)
-      -- highlight = {
-      --     icon = '', -- ''
-      --     group = 'Comment',
-      -- }
-    }
   },
   -- ChatGPT
   {
