2d822966beeae229ececa00dbfbf83fc44d2b1c2
pogopaule <fabian.schwarzer@node.energy>
2024-08-16 21:36:20 +0200

    feat(nvim): improve workspace symbols search

--- nvim/lua/config/plugins/telescope.lua
+++ nvim/lua/config/plugins/telescope.lua
@@ -22,6 +22,11 @@ local find_django_others = function()
   end
 end
 
+local function telescope_dynamic_workspace_symbols_under_cursor()
+  local word = vim.fn.expand('<cword>')
+  vim.cmd('Telescope lsp_workspace_symbols query=' .. word)
+end
+
 return {
   {
     'https://github.com/nvim-telescope/telescope.nvim',
@@ -32,22 +37,24 @@ return {
       'https://github.com/Marskey/telescope-sg',
     },
     keys = {
-      { '<leader>ff',  '<CMD>Telescope find_files<CR>',                    desc = 'Files' },
-      { '<leader>fh',  '<CMD>Telescope help_tags<CR>',                     desc = 'Help' },
-      { '<leader>fa',  '<CMD>Telescope oldfiles<CR>',                      desc = 'Old Files' },
-      { '<leader>fd',  find_django_others,                                 desc = 'Django Other Files' },
-      { '<leader>fk',  '<CMD>Telescope keymaps<CR>',                       desc = 'Keymaps' },
-      { '<leader>fS',  '<CMD>Telescope spell_suggest<CR>',                 desc = 'Spelling' },
-      { '<leader>fg',  '<CMD>Telescope live_grep<CR>',                     desc = 'Grep' },
-      { '<leader>fG',  '<CMD>Telescope grep_string<CR>',                   desc = 'Find Word Under Cursor' },
-      { '<leader>fcp', '<CMD>Telescope git_commits<CR>',                   desc = 'Git Commits For Project' },
-      { '<leader>fcb', '<CMD>Telescope git_bcommits<CR>',                  desc = 'Git Commits For Buffer' },
-      { '<leader>fb',  '<CMD>Telescope buffers<CR>',                       desc = 'Buffers' },
-      { '<leader>f:',  '<CMD>Telescope command_history<CR>',               desc = 'Command History' },
-      { '<leader>f/',  '<CMD>Telescope current_buffer_fuzzy_find<CR>',     desc = 'Fuzzy Find In Current Buffer' },
-      { '<leader>fm',  '<CMD>Telescope resume<CR>',                        desc = 'Resume' },
-      { '<leader>fss', '<CMD>Telescope lsp_document_symbols<CR>',          desc = 'Find Document Symbols' },
-      { '<leader>fsw', '<CMD>Telescope lsp_dynamic_workspace_symbols<CR>', desc = 'Find Workspace Symbols' },
+      { '<leader>ff',  '<CMD>Telescope find_files<CR>',                                  desc = 'Files' },
+      { '<leader>fh',  '<CMD>Telescope help_tags<CR>',                                   desc = 'Help' },
+      { '<leader>fa',  '<CMD>Telescope oldfiles<CR>',                                    desc = 'Old Files' },
+      { '<leader>fd',  find_django_others,                                               desc = 'Django Other Files' },
+      { '<leader>fk',  '<CMD>Telescope keymaps<CR>',                                     desc = 'Keymaps' },
+      { '<leader>fS',  '<CMD>Telescope spell_suggest<CR>',                               desc = 'Spelling' },
+      { '<leader>fg',  '<CMD>Telescope live_grep<CR>',                                   desc = 'Grep' },
+      { '<leader>fG',  '<CMD>Telescope grep_string<CR>',                                 desc = 'Find Word Under Cursor' },
+      { '<leader>fcp', '<CMD>Telescope git_commits<CR>',                                 desc = 'Git Commits For Project' },
+      { '<leader>fcb', '<CMD>Telescope git_bcommits<CR>',                                desc = 'Git Commits For Buffer' },
+      { '<leader>fb',  '<CMD>Telescope buffers<CR>',                                     desc = 'Buffers' },
+      { '<leader>f:',  '<CMD>Telescope command_history<CR>',                             desc = 'Command History' },
+      { '<leader>f/',  '<CMD>Telescope current_buffer_fuzzy_find<CR>',                   desc = 'Fuzzy Find In Current Buffer' },
+      { '<leader>fm',  '<CMD>Telescope resume<CR>',                                      desc = 'Resume' },
+      { '<leader>fss', '<CMD>Telescope lsp_document_symbols<CR>',                        desc = 'Document Symbols' },
+      { '<leader>fsw', '<CMD>Telescope lsp_dynamic_workspace_symbols<CR>',               desc = 'Workspace Symbols' },
+      { '<leader>fsc', '<CMD>Telescope lsp_dynamic_workspace_symbols symbols=class<CR>', desc = 'Classes in Workspace' },
+      { '<leader>fsu', telescope_dynamic_workspace_symbols_under_cursor,                 desc = 'Symbol under Cursor in Workspace' },
     },
     config = function()
       local telescope = require('telescope')
