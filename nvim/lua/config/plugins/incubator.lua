59ba1c93ea11a4d2851e93ffa625776e9f1530b9
pogopaule <fabian.schwarzer@node.energy>
2024-08-16 21:35:43 +0200

    feat(nvim): add some plugins to the incubator

--- nvim/lua/config/plugins/incubator.lua
+++ nvim/lua/config/plugins/incubator.lua
@@ -1,5 +1,55 @@
 return {
   {
+    'https://github.com/tomasky/bookmarks.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require('bookmarks').setup {
+        -- sign_priority = 8,  --set bookmark sign priority to cover other sign
+        save_file = vim.fn.expand "$HOME/.bookmarks", -- bookmarks save file path
+        keywords = {
+          ["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
+          ["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
+          ["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
+          ["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
+        },
+        on_attach = function(bufnr)
+          local bm = require "bookmarks"
+          local map = vim.keymap.set
+          map("n", "mm", bm.bookmark_toggle) -- add or remove bookmark at current line
+          map("n", "mi", bm.bookmark_ann) -- add or edit mark annotation at current line
+          map("n", "mc", bm.bookmark_clean) -- clean all marks in local buffer
+          map("n", "mn", bm.bookmark_next) -- jump to next mark in local buffer
+          map("n", "mp", bm.bookmark_prev) -- jump to previous mark in local buffer
+          map("n", "ml", bm.bookmark_list) -- show marked file list in quickfix window
+          map("n", "mx", bm.bookmark_clear_all) -- removes all bookmarks
+        end
+      }
+    end
+  },
+  {
+    'https://github.com/stevearc/aerial.nvim',
+    opts = {},
+    event = 'BufRead',
+    keys = {
+      { '<leader>to', '<CMD>AerialToggle<CR>',    desc = 'Aerial' },
+      { '<leader>tO', '<CMD>AerialNavToggle<CR>', desc = 'AerialNav' },
+    },
+    cmd = { 'AerialToggle', 'AerialPrev', 'AerialNext' },
+    config = {
+      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+    }
+  },
+  {
+    "https://github.com/hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+  },
+  {
     'https://github.com/andythigpen/nvim-coverage',
     event = 'VeryLazy',
     config = {
@@ -42,7 +92,13 @@ return {
     "https://github.com/RRethy/vim-illuminate",
     event = "BufReadPost",
     config = function()
-      require('illuminate').configure()
+      require('illuminate').configure({
+        filetypes_denylist = {
+          'Outline',
+          'aerial-nav',
+          'aerial',
+        },
+      })
     end,
   },
   {
