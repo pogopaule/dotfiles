68795a0224763bcfade0016162755a4eb960212c
pogopaule <fabian.schwarzer@node.energy>
2024-09-08 08:48:24 +0200

    chore(nvim): cleanup todo comments

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,15 +1,9 @@
 -- TODO: checkout plugins
 -- https://github.com/Pocco81/DAPInstall.nvim
 -- https://github.com/jose-elias-alvarez/typescript.nvim additional lsp features for typescript, mainly imports related
--- https://github.com/ms-jpq/chadtree alternative to nvim-tree?
--- https://github.com/danymat/neogen generate annotations and documentation
--- https://github.com/ThePrimeagen/harpoon naviage favorite locations
--- https://github.com/mizlan/iswap.nvim swapping powered by treesitter
--- https://github.com/Vonr/align.nvim replace Tabularize?
--- https://github.com/jakewvincent/mkdnflow.nvim
 -- as a replacement for null-ls: https://github.com/nvimtools/none-ls.nvim
 -- lazyvim uses https://github.com/mfussenegger/nvim-lint and https://github.com/stevearc/conform.nvim instead of none-ls
--- https://github.com/nvim-telescope/telescope-live-grep-args.nvim
+-- https://github.com/chrisgrieser/nvim-scissors
 
 
 -- has to be defined here so keys activation work properly
