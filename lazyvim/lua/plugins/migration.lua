eeac98404907fd99fd29e29308135d9528547543
pogopaule <fabian.schwarzer@node.energy>
2024-10-07 19:05:47 +0200

    feat(nvim): couple of migration changes

--- lazyvim/lua/plugins/migration.lua
+++ lazyvim/lua/plugins/migration.lua
@@ -109,40 +109,44 @@ return {
   --   },
   -- },
 
-  -- {
-  --   "epwalsh/obsidian.nvim",
-  --   version = "*", -- recommended, use latest release instead of latest commit
-  --   lazy = true,
-  --   ft = "markdown",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --   },
-  --   opts = {
-  --     workspaces = {
-  --       {
-  --         name = "secondbrain",
-  --         path = "~/secondbrain",
-  --       },
-  --     },
-  --     templates = {
-  --       subdir = "4 templates",
-  --     },
-  --     disable_frontmatter = true,
-  --     note_id_func = function(title)
-  --       return title
-  --     end,
-  --     notes_subdir = "0 Inbox",
-  --     new_notes_location = "notes_subdir",
-  --   },
-  --   keys = {
-  --     { "<leader>og", "<cmd>ObsidianSearch<CR>", desc = "Grep" },
-  --     { "<leader>fo", "<cmd>ObsidianQuickSwitch<CR>", desc = "Find File" },
-  --     { "<leader>oo", "<cmd>ObsidianOpen<CR>", desc = "Open in Obsidian" },
-  --     { "<leader>of", "<cmd>ObsidianFollowLink<CR>", desc = "Follow Link" },
-  --     { "<leader>ot", "<cmd>ObsidianTemplate<CR>", desc = "Insert Template" },
-  --     { "<leader>No", "<cmd>ObsidianNew<CR>", desc = "Obsidian" },
-  --   },
-  -- },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "secondbrain",
+          path = "~/secondbrain",
+        },
+      },
+      templates = {
+        subdir = "4 templates",
+      },
+      disable_frontmatter = true,
+      note_id_func = function(title)
+        return title
+      end,
+      notes_subdir = "0 Inbox",
+      new_notes_location = "notes_subdir",
+    },
+    cmd = {
+      "ObsidianQuickSwitch",
+    },
+    keys = {
+      { "<leader>o", "", desc = "+obsidian" },
+      { "<leader>og", "<cmd>ObsidianSearch<CR>", desc = "Grep" },
+      { "<leader>fo", "<cmd>ObsidianQuickSwitch<CR>", desc = "Find File" },
+      { "<leader>oo", "<cmd>ObsidianOpen<CR>", desc = "Open in Obsidian" },
+      { "<leader>of", "<cmd>ObsidianFollowLink<CR>", desc = "Follow Link" },
+      { "<leader>ot", "<cmd>ObsidianTemplate<CR>", desc = "Insert Template" },
+      { "<leader>on", "<cmd>ObsidianNew<CR>", desc = "Obsidian" },
+    },
+  },
 
   {
     "Wansmer/treesj",
