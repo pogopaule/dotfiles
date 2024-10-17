39b596f497e976fcdccf38b222078f2f9a17770b
pogopaule <fabian.schwarzer@node.energy>
2024-10-17 07:09:38 +0200

    feat(nvim): add toggle option for copilot completions

--- lazyvim/lua/plugins/overrides.lua
+++ lazyvim/lua/plugins/overrides.lua
@@ -88,4 +88,24 @@ return {
       },
     },
   },
+  {
+    "zbirenbaum/copilot-cmp",
+    opts = function()
+      local copilot_toggle = require("lazyvim.util.toggle").wrap({
+        name = "Copilot Completion",
+        get = function()
+          return not require("copilot.client").is_disabled()
+        end,
+        set = function(state)
+          if state then
+            require("copilot.command").enable()
+          else
+            require("copilot.command").disable()
+          end
+        end,
+      })
+
+      LazyVim.toggle.map("<leader>at", copilot_toggle)
+    end,
+  },
 }
