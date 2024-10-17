a2d0f98469de09569cd1afd1192b6985ae7f06f2
pogopaule <fabian.schwarzer@node.energy>
2024-10-17 07:10:46 +0200

    feat(nvim): replace jk with jj
    works better with home row mods

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -2,8 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- exit insert mode by pressing jk
-vim.keymap.set("i", "jk", "<ESC>")
+-- exit insert mode by pressing jj
+vim.keymap.set("i", "jj", "<ESC>")
 
 vim.keymap.set({ "n", "v", "i" }, "<C-PageUp>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
 vim.keymap.set({ "n", "v", "i" }, "<C-PageDown>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
