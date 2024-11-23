8d5725d7abe236263ca083d18a7452e8aa233812
pogopaule <fabian.schwarzer@node.energy>
2024-11-23 14:06:03 +0100

    feat(nvim): add notification when macro recording starts
    I often accidentally trigger a macro recording

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local notify = require("notify")
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  callback = function()
+    local register = vim.fn.reg_recording()
+    notify("Recording macro to register: " .. register, "info", {
+      title = "Macro Recording Started",
+    })
+  end,
+})
