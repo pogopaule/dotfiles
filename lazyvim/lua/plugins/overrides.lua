c10e3f02c9f6a204e0253b260f9d1cace8b23285
pogopaule <fabian.schwarzer@node.energy>
2024-11-23 14:04:45 +0100

    fix(nvim): remove dial
    doesn't work as well as expected and mappings interfere with going
    forward Ctrl-I

--- lazyvim/lua/plugins/overrides.lua
+++ lazyvim/lua/plugins/overrides.lua
@@ -10,16 +10,6 @@ local function dial(increment, g)
 end
 return {
   {
-    "monaqa/dial.nvim",
-    -- stylua: ignore
-    keys = {
-      { "<C-i>", function() return dial(true) end, expr = true, desc = "Increment", mode = { "n", "v" } },
-      { "<C-x>", function() return dial(false) end, expr = true, desc = "Decrement", mode = { "n", "v" } },
-      { "g<C-i>", function() return dial(true, true) end, expr = true, desc = "Increment", mode = { "n", "v" } },
-      { "g<C-x>", function() return dial(false, true) end, expr = true, desc = "Decrement", mode = { "n", "v" } },
-    },
-  },
-  {
     "nvimdev/dashboard-nvim",
     opts = {
       config = {
