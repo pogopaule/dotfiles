985d0d6eaaf335f7d77f9a46d0da91a55016dc18
pogopaule <fabian.schwarzer@node.energy>
2024-12-14 08:14:31 +0100

    feat(nvim): add treewalker to incubator

--- lazyvim/lua/plugins/incubator.lua
+++ lazyvim/lua/plugins/incubator.lua
@@ -6,4 +6,16 @@ return {
     event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
     opts = {},
   },
+  {
+    "aaronik/treewalker.nvim",
+    opts = {
+      highlight = true, -- default is false
+    },
+    keys = {
+      { "<A-j>", "<cmd>Treewalker Down<CR>", desc = "Treewalker Down" },
+      { "<A-k>", "<cmd>Treewalker Up<CR>", desc = "Treewalker Up" },
+      { "<A-h>", "<cmd>Treewalker Left<CR>", desc = "Treewalker Left" },
+      { "<A-l>", "<cmd>Treewalker Right<CR>", desc = "Treewalker Right" },
+    },
+  },
 }
