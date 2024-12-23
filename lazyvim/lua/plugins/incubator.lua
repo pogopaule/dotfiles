e7069b94bdc2d87a1234bd5b39202bad6f292ad1
pogopaule <fabian.schwarzer@node.energy>
2024-12-23 16:40:48 +0100

    chore(nvim): remove auto-save

--- lazyvim/lua/plugins/incubator.lua
+++ lazyvim/lua/plugins/incubator.lua
@@ -1,12 +1,5 @@
 return {
   {
-    "okuuva/auto-save.nvim",
-    version = "*", -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
-    cmd = "ASToggle", -- optional for lazy loading on command
-    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
-    opts = {},
-  },
-  {
     "aaronik/treewalker.nvim",
     opts = {
       highlight = true, -- default is false
